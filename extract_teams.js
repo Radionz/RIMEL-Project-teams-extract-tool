2d2923a7b11a4c329e603e2968db23b19568b271
Dorian BLANC <blanc.dorian@etu.unice.fr>
2017-01-24 10:14:45 +0100

    big clean du projet

--- extract_teams.js
+++ extract_teams.js
@@ -1,21 +1,42 @@
+// Global variables
+var table,
+members,
+map_options,
+map,
+markers,
+geocoder;
+
 $( document ).ready(function() {
 
-  $("#results").hide();
+  initTable();
+  initMap();
 
-  // INIT Tables
-  var table = $('#dataTables-teams').DataTable({ responsive: true});
+  if (typeof teamLocations !== 'undefined' && typeof source !== 'undefined') {
+    extractDataFromTeam(source, teamLocations);
+  }
+
+});
+
+function initTable() {
+  // INIT Table
+  table = $('#dataTables-teams').DataTable({ responsive: true});
   $(table.tables().containers()).hide();
   $("#table-loading").hide();
-  // $('#input_url_extract_teams').focus();
+}
 
+function initMap() {
   // INIT Map
-  var markers = [];
-  var map_options = {
+  markers = [];
+  map_options = {
     center: {lat: 43.6155793, lng: 7.0696861},
     zoom: 2
   };
-  var map;
-  // var map = new google.maps.Map(document.getElementById('map'), map_options);
+  // map = new google.maps.Map($("#map"), map_options);
+  geocoder = new google.maps.Geocoder();
+}
+
+function extractDataFromTeam(source, teamLocations) {
+  $("#results").hide();
 
   $('#submit_url_extract_teams').click(function(event) {
 
@@ -38,67 +59,82 @@ $( document ).ready(function() {
       url = "http://spring.io/team.html";
     }
 
-    if(map == undefined) {
-      map = new google.maps.Map(document.getElementById('map'), map_options);
+    if(typeof map === undefined) {
+      map = new google.maps.Map($("#map"), map_options);
     }
+
+    members = [];
 
     var $members = $(source).filter('section'),
     memberNb = $members.length,
     membersPositionsWords = [],
     membersLatLng = [];
-    var geocoder = new google.maps.Geocoder();
     $members.filter('section').each(function(index, member) {
-      var $member = $(member),
-      latitude = 'NA',
-      longitude = 'NA',
-      markerId = 0,
-      marker = null,
-      github = "",
-      rowNode = null,
-      progress = Math.floor(((index + 1) / memberNb) * 100);
+      var $member = $(member);
 
-      $("#table-loading > p > span").text(progress+'%');
-      $('#table-loading > .progress > .progress-bar').css('width', progress+'%').attr('aria-valuenow', progress);
+      var member = {};
+      member.latitude = 'NA';
+      member.longitude = 'NA';
+      member.latLng = null;
+      member.markerId = 0;
+      member.marker = null;
+      member.memberId = $member.attr('data-member-id');
+      member.github = $member.find('.team-member--social--icon.github-small').attr('href');
+      member.name = $member.find('.team-member--name').text();
+      member.position = $member.find('.team-member--position').text();
+      member.location = $member.find('.team-member--location').text();
 
+      console.log(member);
+
+      var rowNode = null;
+
+      // Find member's latitude & longitude
       $.each(teamLocations, function(i, teamLocation) {
-        if (teamLocation.memberId == $member.attr('data-member-id')) {
-          latitude = teamLocation.latitude;
-          longitude = teamLocation.longitude;
+        if (teamLocation.memberId == member.memberId) {
 
-          marker = new google.maps.Marker({
-            position: new google.maps.LatLng(latitude,longitude),
+          member.latitude = teamLocation.latitude;
+          member.longitude = teamLocation.longitude;
+          member.latLng = new google.maps.LatLng(member.latitude, member.longitude)
+          membersLatLng.push(member.latLng);
+
+          member.marker = new google.maps.Marker({
+            position: member.latLng,
             title: teamLocation.name,
             map: map,
             animation: google.maps.Animation.DROP
           });
-          markerId = markers.push(marker);
+          member.markerId = markers.push(member.marker);
+
           return;
         }
       });
 
-      if (marker != null) {
-        membersLatLng.push(marker.getPosition());
-      }
-
-      var memberPositionWords = $member.find('.team-member--position').text().split(' ');
-      Array.prototype.push.apply(membersPositionsWords,memberPositionWords);
-
-      github = $member.find('.team-member--social--icon.github-small').attr('href');
-      rowNode = table.row.add( [
-        $member.find('.team-member--name').text(),
-        $member.find('.team-member--position').text(),
-        $member.find('.team-member--location').text(),
-        latitude + ' / ' + longitude + '<button type="button" class="btn btn-default btn-xs pull-right" id="marker_'+markerId+'">Show on map</button>',
-        '<a href="' + github + '">Github of ' + github.replace('https://github.com/','') + '</a>'
+      rowNode = table.row.add([
+        member.name,
+        member.position,
+        member.location,
+        member.latitude + ' / ' + member.longitude + '<button type="button" class="btn btn-default btn-xs pull-right" id="marker_'+member.markerId+'">Show on map</button>',
+        '<a href="' + member.github + '">Github of ' + member.github.replace('https://github.com/','') + '</a>'
       ]).draw().node();
 
-      $('button[id="marker_'+markerId+'"]').on('click', function() {
-        map.panTo(marker.getPosition());
+      $('button[id="marker_'+member.markerId+'"]').on('click', function() {
+        map.panTo(member.marker.getPosition());
       });
 
-      if (latitude == 'NA') {
+      if (member.latitude == 'NA'||  member.longitude == 'NA') {
         $(rowNode).addClass('warning');
       }
+
+      member.memberPositionWords = member.position.split(' ');
+      Array.prototype.push.apply(membersPositionsWords,member.memberPositionWords);
+
+      members.push(member);
+
+      // Update progress bar
+      var progress = Math.floor(((index + 1) / memberNb) * 100);
+      $("#table-loading > p > span").text(progress+'%');
+      $('#table-loading > .progress > .progress-bar').css('width', progress+'%').attr('aria-valuenow', progress);
+
       if (progress >= 100) {
         $("#table-loading").hide();
         $("#map").show();
@@ -264,33 +300,5 @@ $( document ).ready(function() {
     //   })
     //   .text(function(d) { return d.text; });
     // }
-
-    /*
-    *TODO: get code from url, NOT WORKING BECAUSE OF CORS ERRORS :(
-    */
-
-    // $('#iframe_extract_teams').on('load', function(){
-    //   console.log(document.getElementById('iframe_extract_teams').contentWindow.document.body.innerHTML);
-    // });
-    // $("#iframe_extract_teams").attr('src',url);
-    //
-    // $.ajax({
-    //   url: url,
-    //   cache: false,
-    //   dataType: 'html',
-    //   beforeSend: function( xhr ) {
-    //     xhr.overrideMimeType( "xml/text" );
-    //   }
-    // })
-    // .done(function( data ) {
-    //   console.log( data );
-    // })
-    // .fail(function() {
-    //   console.log( "error" );
-    // })
-    // .always(function() {
-    //   console.log( "complete" );
-    // });;
-
   });
-});
+}
